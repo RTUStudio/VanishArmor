5a8d4b4556ea064bca01b816bd272a415765bd63
IPECTER 이팩터 <80433772+IPECTER@users.noreply.github.com>
2024-12-31 14:54:15 +0900

    Fix: isHideOther, isHideFromOther

--- src/main/java/com/github/ipecter/rtustudio/varmor/commands/Command.java
+++ src/main/java/com/github/ipecter/rtustudio/varmor/commands/Command.java
@@ -1,18 +1,27 @@
 package com.github.ipecter.rtustudio.varmor.commands;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.github.ipecter.rtustudio.varmor.manager.ToggleManager;
+import com.github.ipecter.rtustudio.varmor.protocol.wrapper.WrapperPlayServerEntityEquipment;
 import kr.rtuserver.framework.bukkit.api.command.RSCommand;
 import kr.rtuserver.framework.bukkit.api.command.RSCommandData;
 import com.github.ipecter.rtustudio.varmor.VanishArmor;
 import com.github.ipecter.rtustudio.varmor.config.VanishConfig;
 import kr.rtuserver.framework.bukkit.api.utility.player.PlayerChat;
 import kr.rtuserver.framework.bukkit.api.utility.scheduler.CraftScheduler;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Command extends RSCommand<VanishArmor> {
 
     private final VanishConfig config;
     private final ToggleManager manager;
+
+    private final ItemStack empty = new ItemStack(Material.AIR);
 
     public Command(VanishArmor plugin) {
         super(plugin, "varmor", true);
@@ -33,7 +42,31 @@ public class Command extends RSCommand<VanishArmor> {
                 manager.on(player.getUniqueId());
                 chat.announce(getSender(), getMessage().get(getSender(), "enable"));
             }
-            CraftScheduler.runLaterAsync(getPlugin(), player::updateInventory, 1);
+            if (config.isHideSelf()) CraftScheduler.runLaterAsync(getPlugin(), player::updateInventory, 1);
+            if (config.isHideOther()) {
+                ProtocolManager pm = ProtocolLibrary.getProtocolManager();
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    WrapperPlayServerEntityEquipment packet = new WrapperPlayServerEntityEquipment();
+                    packet.setEntityID(p.getEntityId());
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.HEAD, empty);
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.CHEST, empty);
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.LEGS, empty);
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.FEET, empty);
+                    pm.sendServerPacket(player, packet.getHandle());
+                }
+            }
+            if (config.isHideFromOther()) {
+                ProtocolManager pm = ProtocolLibrary.getProtocolManager();
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    WrapperPlayServerEntityEquipment packet = new WrapperPlayServerEntityEquipment();
+                    packet.setEntityID(player.getEntityId());
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.HEAD, empty);
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.CHEST, empty);
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.LEGS, empty);
+                    packet.setSlotStackPair(EnumWrappers.ItemSlot.FEET, empty);
+                    pm.sendServerPacket(p, packet.getHandle());
+                }
+            }
             return true;
         } else chat.announce(getSender(), getCommon().getMessage(getSender(), "onlyPlayer"));
         return false;
